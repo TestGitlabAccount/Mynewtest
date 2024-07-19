dcbec7e55fbe518a17c70f85d3726dc8f04f55db
TestGitlabAccount <15241A1207@gmail.com>
2024-07-19 13:44:04 +0530

    Create test22.py

---
+++ test22.py
@@ -0,0 +1,211 @@
+from fastapi import FastAPI, HTTPException
+from aws.rdsMethods import group_rds_clusters_by_vsad, group_rds_instances_by_vsad
+from aws.mskMethods import group_msk_clusters_by_vsad
+from aws.elasticacheMethods import group_elasticache_clusters_by_vsad
+from aws.opensearchMethods import group_opensearch_domains_by_vsad
+
+app = FastAPI()
+
+@app.get("/rds/clusters")
+async def get_rds_clusters_by_vsad():
+    try:
+        vsad_groups = group_rds_clusters_by_vsad()
+        return vsad_groups
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/rds/instances")
+async def get_rds_instances_by_vsad():
+    try:
+        vsad_groups = group_rds_instances_by_vsad()
+        return vsad_groups
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/msk/clusters")
+async def get_msk_clusters_by_vsad():
+    try:
+        vsad_groups = group_msk_clusters_by_vsad()
+        return vsad_groups
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/elasticache/clusters")
+async def get_elasticache_clusters_by_vsad():
+    try:
+        vsad_groups = group_elasticache_clusters_by_vsad()
+        return vsad_groups
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/opensearch/domains")
+async def get_opensearch_domains_by_vsad():
+    try:
+        vsad_groups = group_opensearch_domains_by_vsad()
+        return vsad_groups
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+
+import boto3
+
+def get_vsad_tag(tags):
+    for tag in tags:
+        if tag['Key'] == 'Vsad':
+            return tag['Value']
+    return None
+
+def group_by_vsad(instances):
+    vsad_group = {}
+    for instance in instances:
+        vsad = get_vsad_tag(instance.get('Tags', []))
+        if vsad:
+            if vsad not in vsad_group:
+                vsad_group[vsad] = {
+                    'count': 0,
+                    'instances': []
+                }
+            vsad_group[vsad]['count'] += 1
+            vsad_group[vsad]['instances'].append(instance)
+    return vsad_group
+
+
+
+import boto3
+from .helpers import get_vsad_tag, group_by_vsad
+
+def get_rds_clusters():
+    rds_client = boto3.client('rds')
+    clusters = []
+    paginator = rds_client.get_paginator('describe_db_clusters')
+    for page in paginator.paginate():
+        clusters.extend(page['DBClusters'])
+    return clusters
+
+def get_rds_instances():
+    rds_client = boto3.client('rds')
+    instances = []
+    paginator = rds_client.get_paginator('describe_db_instances')
+    for page in paginator.paginate():
+        instances.extend(page['DBInstances'])
+    return instances
+
+def group_rds_clusters_by_vsad():
+    clusters = get_rds_clusters()
+    return group_by_vsad(clusters)
+
+def group_rds_instances_by_vsad():
+    instances = get_rds_instances()
+    return group_by_vsad(instances)
+
+
+import boto3
+from .helpers import get_vsad_tag, group_by_vsad
+
+def get_msk_clusters():
+    msk_client = boto3.client('kafka')
+    clusters = []
+    paginator = msk_client.get_paginator('list_clusters')
+    for page in paginator.paginate():
+        for cluster in page['ClusterInfoList']:
+            clusters.append(cluster)
+    return clusters
+
+def group_msk_clusters_by_vsad():
+    clusters = get_msk_clusters()
+    return group_by_vsad(clusters)
+
+
+
+import boto3
+from .helpers import get_vsad_tag, group_by_vsad
+
+def get_elasticache_clusters():
+    elasticache_client = boto3.client('elasticache')
+    clusters = []
+    paginator = elasticache_client.get_paginator('describe_cache_clusters')
+    for page in paginator.paginate():
+        clusters.extend(page['CacheClusters'])
+    return clusters
+
+def group_elasticache_clusters_by_vsad():
+    clusters = get_elasticache_clusters()
+    return group_by_vsad(clusters)
+
+
+import boto3
+from .helpers import get_vsad_tag, group_by_vsad
+
+def get_opensearch_domains():
+    opensearch_client = boto3.client('opensearch')
+    domains = []
+    paginator = opensearch_client.get_paginator('list_domain_names')
+    for page in paginator.paginate():
+        for domain_info in page['DomainNames']:
+            domain_name = domain_info['DomainName']
+            domain_details = opensearch_client.describe_domain(DomainName=domain_name)
+            domains.append(domain_details['DomainStatus'])
+    return domains
+
+def group_opensearch_domains_by_vsad():
+    domains = get_opensearch_domains()
+    return group_by_vsad(domains)
+
+
+from fastapi import FastAPI, HTTPException
+from aws.rdsMethods import group_rds_clusters_by_vsad, group_rds_instances_by_vsad
+from aws.mskMethods import group_msk_clusters_by_vsad
+from aws.elasticacheMethods import group_elasticache_clusters_by_vsad
+from aws.opensearchMethods import group_opensearch_domains_by_vsad
+
+app = FastAPI()
+
+@app.get("/rds/clusters")
+async def get_rds_clusters_by_vsad():
+    try:
+        vsad_groups = group_rds_clusters_by_vsad()
+        return vsad_groups
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/rds/instances")
+async def get_rds_instances_by_vsad():
+    try:
+        vsad_groups = group_rds_instances_by_vsad()
+        return vsad_groups
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/msk/clusters")
+async def get_msk_clusters_by_vsad():
+    try:
+        vsad_groups = group_msk_clusters_by_vsad()
+        return vsad_groups
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/elasticache/clusters")
+async def get_elasticache_clusters_by_vsad():
+    try:
+        vsad_groups = group_elasticache_clusters_by_vsad()
+        return vsad_groups
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/opensearch/domains")
+async def get_opensearch_domains_by_vsad():
+    try:
+        vsad_groups = group_opensearch_domains_by_vsad()
+        return vsad_groups
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
