306cb3cb8de1dfe550955fb6a1b0447da6467964
TestGitlabAccount <15241A1207@gmail.com>
2024-10-10 19:03:37 +0530

    Update New.py

--- New.py
+++ New.py
@@ -50,3 +50,66 @@ if __name__ == '__main__':
             print(f"  ALB ARN: {alb['LoadBalancerArn']}")
             print(f"  Tags: {alb['Tags']}")
         print("\n")
+
+
+
+
+
+import boto3
+
+def get_alb_tags_by_vsad():
+    client = boto3.client('elbv2')
+    response = client.describe_load_balancers()
+    load_balancers = response['LoadBalancers']
+
+    vsad_data = {}
+
+    for lb in load_balancers:
+        if lb['Type'] != 'application':  # Filter only ALBs
+            continue
+
+        lb_name = lb['LoadBalancerName']
+        lb_arn = lb['LoadBalancerArn']
+        tags_response = client.describe_tags(ResourceArns=[lb_arn])
+        tags = {tag['Key']: tag['Value'] for tag in tags_response['TagDescriptions'][0]['Tags']}
+        vsad = tags.get('VSAD')
+
+        if vsad:
+            if vsad not in vsad_data:
+                vsad_data[vsad] = []
+            vsad_data[vsad].append({
+                'LoadBalancerName': lb_name,
+                'LoadBalancerArn': lb_arn,
+                'Tags': tags
+            })
+
+    return vsad_data
+
+
+def get_nlb_tags_by_vsad():
+    client = boto3.client('elbv2')
+    response = client.describe_load_balancers()
+    load_balancers = response['LoadBalancers']
+
+    vsad_data = {}
+
+    for lb in load_balancers:
+        if lb['Type'] != 'network':  # Filter only NLBs
+            continue
+
+        lb_name = lb['LoadBalancerName']
+        lb_arn = lb['LoadBalancerArn']
+        tags_response = client.describe_tags(ResourceArns=[lb_arn])
+        tags = {tag['Key']: tag['Value'] for tag in tags_response['TagDescriptions'][0]['Tags']}
+        vsad = tags.get('VSAD')
+
+        if vsad:
+            if vsad not in vsad_data:
+                vsad_data[vsad] = []
+            vsad_data[vsad].append({
+                'LoadBalancerName': lb_name,
+                'LoadBalancerArn': lb_arn,
+                'Tags': tags
+            })
+
+    return vsad_data
